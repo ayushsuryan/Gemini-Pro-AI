f5a837c18eac8304871b56f576f68dcedf570ed2
Ayush Suryan <suryanayush@gmail.com>
2024-08-30 14:37:22 +0530

    sidebar 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div>
-      <Sidebar></Sidebar>
+      <Sidebar />
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,50 @@
 import styles from "../styles/sidebar.module.css";
+import { assets } from "../assets/assets";
+import { useState } from "react";
+
 export default function Sidebar() {
-  return <div className={styles.sidebar}>Hello I am sidebar</div>;
+  const [extended, setExtended] = useState(false);
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.top}>
+        <div className={styles.menu}>
+          <img
+            onClick={(e) => {
+              setExtended((prev) => !prev);
+            }}
+            src={assets.menu_icon}
+            alt="hamburger"
+          />
+        </div>
+        <div className={styles.newChat}>
+          <img src={assets.plus_icon} alt="new-chat" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {extended ? (
+          <div className={styles.recent}>
+            <p className={styles.recentTitle}>Recent</p>
+            <div className={styles.recentEntry}>
+              <img src={assets.message_icon} alt="message-icon" />
+              <p>What is react ...</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+
+      <div className={styles.bottom}>
+        <div className={`${styles.bottomItem} ${styles.recentEntry}`}>
+          <img src={assets.question_icon} alt="question-icon" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className={`${styles.bottomItem} ${styles.recentEntry}`}>
+          <img src={assets.history_icon} alt="question-icon" />
+          {extended ? <p>History</p> : null}
+        </div>
+        <div className={`${styles.bottomItem} ${styles.recentEntry}`}>
+          <img src={assets.setting_icon} alt="question-icon" />
+          {extended ? <p>Setting</p> : null}
+        </div>
+      </div>
+    </div>
+  );
 }
